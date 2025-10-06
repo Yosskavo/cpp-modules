3491d52087186b691e30e4b3c9a54147d5a2e050
Yassine El motaouakkel <yel-mota@c1r8p4.1337.ma>
2025-10-06 16:36:27 +0100

    ex00

---
+++ cpp-module-00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    while (av[i])
+    {
+        j = 0;
+        while (av[i][j])
+        {
+            std::cout << (char)std::toupper(av[i][j]);
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
+    return (0);
+}
