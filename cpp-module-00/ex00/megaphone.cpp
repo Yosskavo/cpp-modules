36a33c3ab92cf240efc3efdfaec1ad3eb47bdcc7
yel-mota <yel-mota@c2r6p14.1337.ma>
2025-12-02 09:26:39 +0100

    tes

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -1,25 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mota <yel-mota@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 08:36:16 by yel-mota          #+#    #+#             */
+/*   Updated: 2025/12/01 08:36:27 by yel-mota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <cctype>
 
 int main(int ac, char **av)
 {
-    int i;
-    int j;
-
-    i = 1;
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    while (av[i])
-    {
-        j = 0;
-        while (av[i][j])
-        {
-            std::cout << (char)std::toupper(av[i][j]);
-            j++;
-        }
-        i++;
-    }
-    std::cout << std::endl;
-    return (0);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; av[i]; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+		{
+			std::cout << static_cast<char>(std::toupper(av[i][j]));
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
-
